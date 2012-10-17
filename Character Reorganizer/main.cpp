b03fa07750301c0504c104957a87a7925a01004b
Stephen Moon <student@smoon2016.hotchkiss.org>
2012-10-17 15:46:49 -0400

    Initial Commit

---
+++ Character Reorganizer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Character Reorganizer
+//
+//  Created by Stephen Moon on 10/17/12.
+//  Copyright (c) 2012 Stephen Moon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
